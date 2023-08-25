832ad43d037684ce5205fcad7eea9db128e4769e
anshraiyani <anshraiyani009@gmail.com>
2023-08-25 11:06:23 +0530

    10

--- lib/main.dart
+++ lib/main.dart
@@ -1,11 +1,14 @@
-import 'package:app1_2/homePage.dart';
 import 'package:flutter/material.dart';
 
 void main() {
   runApp(
     MaterialApp(
       debugShowCheckedModeBanner: false,
-      home: HomePage(),
+      home: SafeArea(
+        child: Scaffold(
+
+        ),
+      ),
     )
   );
 }
